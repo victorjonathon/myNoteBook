75d374bac07952f1797e6cad76a7ae26b35f2fc3
Vikas <victorjonathon@gmail.com>
2022-02-23 15:30:19 +0530

    change backend PORT to 5000; User save refectoring

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,22 +4,30 @@ const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 const { ResultWithContext } = require('express-validator/src/chain');
 
-router.post('/',[
+//Create user using: POST "/api/auth/createuser". No login required.
+router.post('/createuser',[
     body('name', 'Name should contain minimum 3 letters.').isLength({ min: 3 }),
     body('email', 'Please enter a valid email.').isEmail(),
     body('password', 'Password must have minimum 5 letters').isLength({ min: 5 })
-], (req, res) => {
+], async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-      }).then(user => res.json(user))
-      .catch(err => res.json({error: `Email already exist.`}));
+    let user = await User.findOne({email: req.body.email });
+    
+    if(user){
+        res.status(400).json('A user with this Email already exist.')
+    }else{
+        user = User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        }).then(user => res.json(user))
+        .catch(err=> { console.log(err.message)
+            res.json('Some error occurred.')});
+    }
 
 });
 
